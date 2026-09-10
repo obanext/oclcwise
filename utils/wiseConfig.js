35022208a59cb3c405f35a90e5cd5296f001c8a4
obanext <93115438+obanext@users.noreply.github.com>
2026-09-10 06:24:44 +0200

    Update WISE_BASE_URL and WISE_DEFAULT_SCOPE values

--- utils/wiseConfig.js
+++ utils/wiseConfig.js
@@ -1,8 +1,8 @@
-export const WISE_BASE_URL = "https://bibliotheek-accept4.wise.oclc.org/restapi";
+export const WISE_BASE_URL = "https://bibliotheek-accept1.wise.oclc.org/restapi";
 export const WISE_BRANCH_ID = "1000";
 export const WISE_CLIENT_TYPE = "default";
 export const WISE_DEFAULT_PERSPECTIVE_ID = "3682";
-export const WISE_DEFAULT_SCOPE = "title";
+export const WISE_DEFAULT_SCOPE = "anything";
 export const WISE_DEFAULT_SORT = "2910";
 export const WISE_TIMEOUT_MS = 15_000;
 
